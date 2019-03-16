3b3c61a280795c17ef5e009a59e035417f00a64c
Stian <Stian@Stians-iMac.local>
2019-03-16 13:18:22 +0100

    Initial project upload

---
+++ Stians/Kernel/init.m
@@ -0,0 +1,5 @@
+(* Wolfram Language Init File *)
+
+Get["Stians`FileOperation`"]
+Get["Stians`GroupTheory`"]
+Get["Stians`Miscellaneous`"]
---
+++ build/Stians/PacletInfo.m
@@ -0,0 +1,16 @@
+(* Paclet Info File *)
+
+(* created 2019/01/08*)
+
+Paclet[
+    Name -> "Stians",
+    Version -> "0.1.0",
+    MathematicaVersion -> "10+",
+    Creator -> "Stian Penev Ramsnes",
+    Extensions -> 
+        {
+            {"Documentation", Language -> "English"}
+        }
+]
+
+
